73544eb155bbfaf820deade2a9864e9e20c67518
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 17:03:19 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
 var playlist = new Object({Beatles: 'Abbey Road'});
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist['Slowdive'] = 'Alison'
+  playlist['My Bloody Valentine'] = 'Sometimes'
+}
