9c2a9c7dbe3d73145247830a1c5a4800904fa1e2
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 16:46:09 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {Beatles: "Blackbird"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign ({}, {"artist"}, {"song title"})
+  Object.assign ({}, "artist", "song title")
 }
