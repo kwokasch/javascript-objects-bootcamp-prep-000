d5afeac151952f5ee74127c8e9ca04abac43be8f
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 16:55:35 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,1 @@
-var playlist = {Beatles: "Blackbird"}
-
-function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign ({}, 'Phil Ochs', "Here's to the State of Mississippi")
-}
-
-function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
-  playlist;
-}
+var playlist = new Object(artistName, songTitle);
