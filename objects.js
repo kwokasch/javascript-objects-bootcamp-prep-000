1b1c7d13601f14db252578a83a0122712dcb108e
Katie Wokasch <katie.wokasch@gmail.com>
2019-07-21 17:08:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object({artistName: 'Abbey Road'});
+var playlist = new Object({artist: 'song'});
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle
